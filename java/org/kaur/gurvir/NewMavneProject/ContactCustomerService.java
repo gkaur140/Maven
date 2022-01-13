0b56607a1c2bfee9147f6d36844323c01ebd154d
gkaur140 <gurvirgill266@yahoo.com>
2022-01-13 12:19:15 -0500

    modified

--- java/org/kaur/gurvir/NewMavneProject/ContactCustomerService.java
+++ java/org/kaur/gurvir/NewMavneProject/ContactCustomerService.java
@@ -1,11 +1,15 @@
 package org.kaur.gurvir.NewMavneProject;
 
+
+
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+//import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -33,29 +37,37 @@ public class ContactCustomerService {
 	@Test
 	public void verifyingSendMessageInForm() {
 
-		WebElement contactUs = wd.findElement(By.cssSelector("[title=\"Contact Us\"]"));
+		WebElement contactUs = wd.findElement(By.cssSelector("div a[title='Contact Us']"));
 
 		contactUs.click();
 
-		WebElement subjectHeading = wd.findElement(By.id("uniform-id_contact"));
+		WebElement subjectHeading = wd.findElement(By.cssSelector("select[id=id_contact]"));
 
-		subjectHeading.click();
+		//subjectHeading.click();
+		Select select = new Select(subjectHeading);
 
-		WebElement customerService = wd.findElement(By.cssSelector("select option[value='2']"));
+		// Select using index method
+		// select.selectByIndex(0);
+		select.selectByVisibleText("Customer service");
 
-		customerService.click();
+		//WebElement customerService = wd.findElement(By.cssSelector("select option[value='2']"));
+		//customerService.click();
+
 		WebElement email = wd.findElement(By.cssSelector("input[id=email]"));
-
 		email.sendKeys("gurvirgill2@yahoo.com");
+
 		WebElement referenceId = wd.findElement(By.cssSelector("input[id=id_order]"));
-
 		referenceId.sendKeys("RG58KCV3858");
+
 		WebElement message = wd.findElement(By.cssSelector("textarea[id=message]"));
-
 		message.sendKeys("Not delivered parsel");
-		WebElement send = wd.findElement(By.id("submitMessage"));
 
+		WebElement file = wd.findElement(By.id("fileUpload"));
+		file.sendKeys("C:\\Users\\User\\Downloads\\PHOTO-2022-01-04-09-03-58 (1).jpg");
+
+		WebElement send = wd.findElement(By.id("submitMessage"));
 		send.click();
+
 		WebElement text = wd.findElement(By.cssSelector("p.alert.alert-success"));
 
 		Assert.assertEquals(text.getText(), "Your message has been successfully sent to our team.");
